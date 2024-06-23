dc2f8eb8fc70560b204ef86d163f6e6188f00390
FrznErth <121426632+FrznErth@users.noreply.github.com>
2024-06-23 21:00:36 +0900

    utils

---
+++ control/util.ts
@@ -0,0 +1,29 @@
+import {Course} from './CourseData'
+
+export const PeriodtoStartTimes: { [key: string]: string } = {
+    "1": "8:45",
+    "2": "10:35",
+    "3": "12:30",
+    "4": "14:20",
+    "5": "16:10",
+    "6": "18:00"
+};
+
+
+export const PeriodEndTimes: { [key: string]: string } = {
+    "1": "10:25",
+    "2": "12:15",
+    "3": "14:10",
+    "4": "16:10",
+    "5": "17:50",
+    "6": "19:40"
+};
+
+export const assignPeriodTimees = (course: Course): Course => {
+    const period = course.period;
+
+    course.code.PeriodStartTimes = PeriodStartTimes[period];
+    course.code.PeriodEndTimes = PeriodEndTimes[period];
+
+    return course;
+}
